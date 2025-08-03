5b71c66f3841b1bcf45f1bab74c1ca9ae79ef538
Timothy Grant <timothyleegrant1995@gmail.com>
2025-08-03 00:22:11 -0700

    Created the initial framework for the producer microservice

---
+++ producer_resource/src/main/java/com/example/timothy/producer_resource/ProducerResourceApplication.java
@@ -0,0 +1,13 @@
+package com.example.timothy.producer_resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerResourceApplication.class, args);
+	}
+
+}
---
+++ producer_resource/src/test/java/com/example/timothy/producer_resource/ProducerResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.timothy.producer_resource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
