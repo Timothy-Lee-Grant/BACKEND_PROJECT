61ac4c8b13d338ebfe5a71488aa29c0583c1bcb8
Timothy Grant <timothyleegrant1995@gmail.com>
2025-08-10 01:58:16 -0700

    Added controller functions to consumer_resource to be able to take items away from collection.

--- consumer_resource/src/main/java/com/example/timothy/consumer_resource/controller/Consumer_Controller.java
+++ consumer_resource/src/main/java/com/example/timothy/consumer_resource/controller/Consumer_Controller.java
@@ -1,13 +1,30 @@
 package com.example.timothy.consumer_resource.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+
+import com.example.timothy.consumer_resource.repository.SupplyRepository;
+
+import com.example.timothy.consumer_resource.model.Supply;
 
 @RestController
 @RequestMapping("/consume_resource")
 public class Consumer_Controller {
     
+    private final SupplyRepository repository;
+
+    public Consumer_Controller(SupplyRepository repository) {
+        this.repository = repository;
+    }
+
     @GetMapping("/")
     public String homePage()
     {
@@ -18,5 +35,32 @@ public class Consumer_Controller {
     public String testPage()
     {
         return "This is the test page";
+    }
+
+    @GetMapping("/findAll")
+    public List<Supply> getAllSupplies() {
+        return repository.findAll();
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<String> consumePage(@RequestBody Supply supply)
+    {
+        //find element by name 
+        List<Supply> items = repository.findAll();
+        for (Supply item : items) {
+            //if (item.getName().equalsIgnoreCase(supply.getName()) )
+            if (supply.getName() != null && supply.getName().equalsIgnoreCase(item.getName()))
+            {
+                if (item.getQuantity() < supply.getQuantity())
+                {
+                    repository.deleteById(item.getId());
+                    return ResponseEntity.status(HttpStatus.OK).body("Not enough inventory");
+                }
+                item.setQuantity(item.getQuantity()-supply.getQuantity());
+                repository.save(item);
+                return ResponseEntity.status(HttpStatus.OK).body("Items deleted");
+            }
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Item not found");
     }
 }
--- consumer_resource/src/main/java/com/example/timothy/consumer_resource/repository/SupplyRepository.java
+++ consumer_resource/src/main/java/com/example/timothy/consumer_resource/repository/SupplyRepository.java
@@ -2,6 +2,7 @@ package com.example.timothy.consumer_resource.repository;
 
 import com.example.timothy.consumer_resource.model.Supply;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
 public interface SupplyRepository extends MongoRepository<Supply, String>{
     
 }
