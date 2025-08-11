7201566a1f8f6aa80091fe97ee8e8e60bedf7405
Timothy Grant <timothyleegrant1995@gmail.com>
2025-08-11 01:15:31 -0700

    Added functionality to allow microservices to register with Eureka server.

--- consumer_resource/src/main/java/com/example/timothy/consumer_resource/ConsumerResourceApplication.java
+++ consumer_resource/src/main/java/com/example/timothy/consumer_resource/ConsumerResourceApplication.java
@@ -2,8 +2,10 @@ package com.example.timothy.consumer_resource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConsumerResourceApplication {
 
 	public static void main(String[] args) {
